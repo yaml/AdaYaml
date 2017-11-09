f6025af65fdc155c43b10a6041604ed0b68bf9bc
Felix Krause <contact@flyx.org>
2017-11-09 22:05:13 +0100

    Fixes #6

--- src/implementation/yaml-dom-loading.adb
+++ src/implementation/yaml-dom-loading.adb
@@ -2,12 +2,15 @@
 --  released under the terms of the MIT license, see the file "copying.txt"
 
 with Ada.Containers.Indefinite_Hashed_Maps;
+with Ada.Containers.Vectors;
 with Yaml.Dom.Node;
 with Yaml.Dom.Mapping_Data;
 with Yaml.Dom.Sequence_Data;
 with Yaml.Parser.Stream;
 
 package body Yaml.Dom.Loading is
+   use type Ada.Containers.Count_Type;
+
    package Anchor_Maps is new Ada.Containers.Indefinite_Hashed_Maps
      (Text.Reference, Node_Pointer, Text.Hash, Text."=");
 
@@ -29,94 +32,149 @@ package body Yaml.Dom.Loading is
                           return Mapping_Data.Instance with Import,
      Convention => Ada, Link_Name => "AdaYaml__Mapping_Data__For_Document";
 
+   type Level_Type is record
+      Cur, Key : access Node.Instance;
+   end record;
+
+   package Level_Vectors is new Ada.Containers.Vectors (Positive, Level_Type);
+
    package body Stream_Loading is
-      function Node_From (Target_Document : not null access Document_Instance;
-                          Head : Event; Tail : in out Stream.Instance;
-                          Anchors : in out Anchor_Maps.Map)
-                          return Node_Pointer is
+      procedure Read_Document_Content
+        (Target : not null access Document_Instance;
+         Input : in out Stream.Instance) is
+         Anchors : Anchor_Maps.Map;
+
+         function Start_Node (E : Event; Is_Finished : out Boolean)
+                              return Node_Pointer is
+         begin
+            case E.Kind is
+               when Scalar =>
+                  Is_Finished := True;
+                  return Ret : constant Node_Pointer :=
+                    new Node.Instance'(Kind => Scalar,
+                                       Tag => E.Scalar_Properties.Tag,
+                                       Scalar_Style => E.Scalar_Style,
+                                       Content => E.Content) do
+                     if E.Scalar_Properties.Anchor.Length /= 0 then
+                        Anchors.Include (E.Scalar_Properties.Anchor, Ret);
+                     end if;
+                  end return;
+               when Sequence_Start =>
+                  Is_Finished := False;
+                  return Ret : constant Node_Pointer :=
+                    new Node.Instance'(Kind => Sequence,
+                                       Tag => E.Collection_Properties.Tag,
+                                       Sequence_Style => E.Collection_Style,
+                                       Items => For_Document (Target)) do
+                     if E.Collection_Properties.Anchor.Length /= 0 then
+                        Anchors.Include (E.Collection_Properties.Anchor, Ret);
+                     end if;
+                  end return;
+               when Mapping_Start =>
+                  Is_Finished := False;
+                  return Ret : constant Node_Pointer :=
+                    new Node.Instance'(Kind => Mapping,
+                                       Tag => E.Collection_Properties.Tag,
+                                       Mapping_Style => E.Collection_Style,
+                                       Pairs => For_Document (Target)) do
+                     if E.Collection_Properties.Anchor.Length /= 0 then
+                        Anchors.Include (E.Collection_Properties.Anchor, Ret);
+                     end if;
+                  end return;
+               when Alias =>
+                  declare
+                     Pos : constant Anchor_Maps.Cursor :=
+                       Anchors.Find (E.Target);
+                  begin
+                     if Anchor_Maps.Has_Element (Pos) then
+                        Is_Finished := True;
+                        return Anchor_Maps.Element (Pos);
+                     else
+                        raise Composer_Error with "Unresolvable alias: " &
+                          E.Target.Value.Data.all;
+                     end if;
+                  end;
+               when Annotation_Start =>
+                  raise Composer_Error with
+                    "Annotations not implemented for DOM";
+               when others =>
+                  raise Stream_Error with "Cannot start a node from event: " &
+                    E.Kind'Img;
+            end case;
+         end Start_Node;
+
+         Is_Finished : Boolean;
+         Level : Level_Type := (others => null);
+         Context : Level_Vectors.Vector;
       begin
-         case Head.Kind is
-         when Scalar =>
-            return Ret : constant Node_Pointer :=
-              new Node.Instance'(Kind => Scalar,
-                                 Tag => Head.Scalar_Properties.Tag,
-                                 Scalar_Style => Head.Scalar_Style,
-                                 Content => Head.Content) do
-               if Head.Scalar_Properties.Anchor.Length /= 0 then
-                  Anchors.Include (Head.Scalar_Properties.Anchor, Ret);
-               end if;
-            end return;
-         when Sequence_Start =>
-            return Ret : constant Node_Pointer :=
-              new Node.Instance'(Kind => Sequence,
-                                 Tag => Head.Collection_Properties.Tag,
-                                 Sequence_Style => Head.Collection_Style,
-                                 Items => For_Document (Target_Document)) do
+         Level.Cur := Start_Node (Stream.Next (Input), Is_Finished);
+         loop
+            if Is_Finished then
+               exit when Context.Length = 0;
                declare
-                  New_Head : Event := Stream.Next (Tail);
+                  Parent : Level_Type := Context.Last_Element;
                begin
-                  while New_Head.Kind /= Sequence_End loop
-                     Raw_Append (Ret.Items,
-                                 Node_From (Target_Document, New_Head, Tail,
-                                   Anchors));
-                     New_Head := Stream.Next (Tail);
-                  end loop;
+                  case Parent.Cur.Kind is
+                     when Sequence =>
+                        Raw_Append (Parent.Cur.Items, Level.Cur);
+                     when Mapping =>
+                        if Parent.Key = null then
+                           Parent.Key := Level.Cur.all'Unchecked_Access;
+                        else
+                           Raw_Insert (Parent.Cur.Pairs, Parent.Key, Level.Cur);
+                           Parent.Key := null;
+                        end if;
+                     when Scalar =>
+                        raise Program_Error with
+                          "Internal error: scalar node in DOM context";
+                  end case;
+                  Level := Parent;
+                  Context.Delete_Last;
                end;
-               if Head.Collection_Properties.Anchor.Length /= 0 then
-                  Anchors.Include (Head.Collection_Properties.Anchor, Ret);
-               end if;
-            end return;
-         when Mapping_Start =>
-            return Ret : constant Node_Pointer :=
-              new Node.Instance'(Kind => Mapping,
-                                 Tag => Head.Collection_Properties.Tag,
-                                 Mapping_Style => Head.Collection_Style,
-                                 Pairs => For_Document (Target_Document)) do
-               declare
-                  New_Head : Event := Stream.Next (Tail);
-               begin
-                  while New_Head.Kind /= Mapping_End loop
-                     declare
-                        Key : constant not null access Node.Instance :=
-                          Node_From (Target_Document, New_Head, Tail, Anchors);
-                        Value : constant not null access Node.Instance :=
-                          Node_From (Target_Document, Stream.Next (Tail), Tail,
-                                     Anchors);
-                     begin
-                        Raw_Insert (Ret.Pairs, Key, Value);
-                     end;
-                     New_Head := Stream.Next (Tail);
-                  end loop;
-               end;
-               if Head.Collection_Properties.Anchor.Length /= 0 then
-                  Anchors.Include (Head.Collection_Properties.Anchor, Ret);
-               end if;
-            end return;
-         when Alias =>
+            end if;
             declare
-               Pos : constant Anchor_Maps.Cursor := Anchors.Find (Head.Target);
+               E : constant Event := Stream.Next (Input);
             begin
-               if Anchor_Maps.Has_Element (Pos) then
-                  return Anchor_Maps.Element (Pos);
-               else
-                  raise Composer_Error with "Unresolvable alias: " &
-                    Head.Target.Value.Data.all;
-               end if;
+               case E.Kind is
+                  when Scalar | Sequence_Start | Mapping_Start | Alias |
+                       Annotation_Start =>
+                     Context.Append (Level);
+                     Level := (Cur => Start_Node (E, Is_Finished), Key => null);
+                  when Mapping_End =>
+                     if Level.Cur.Kind /= Mapping then
+                        raise Composer_Error with
+                          "Unexpected mapping end (expected sequence end)";
+                     end if;
+                     if Level.Key /= null then
+                        raise Composer_Error with
+                          "Missing value for key in mapping";
+                     end if;
+                     Is_Finished := True;
+                  when Sequence_End =>
+                     if Level.Cur.Kind /= Sequence then
+                        raise Composer_Error with
+                          "Unexpected sequence end (expected mapping end)";
+                     end if;
+                     Is_Finished := True;
+                  when Annotation_End =>
+                     -- TODO
+                     raise Composer_Error with
+                       "Unexpected annotation end";
+                  when Stream_Start | Document_Start | Document_End |
+                       Stream_End =>
+                     raise Composer_Error with
+                       "Unexpected event inside document: " & E.Kind'Img;
+               end case;
             end;
-         when Annotation_Start =>
-            raise Composer_Error with "Annotations not implemented for DOM";
-         when others =>
-            raise Stream_Error with "Cannot start a node from event: " &
-              Head.Kind'Img;
-         end case;
-      end Node_From;
+         end loop;
+         Target.Root_Node := Level.Cur.all'Unchecked_Access;
+      end Read_Document_Content;
 
       function Load_One (Input : in out Stream.Instance;
                          Pool  : Text.Pool.Reference :=
                            Text.Pool.With_Capacity (Text.Pool.Default_Size))
                              return Document_Reference is
          Head : Event := Stream.Next (Input);
-         Anchors : Anchor_Maps.Map;
       begin
          if Head.Kind /= Stream_Start then
             raise Stream_Error with "Unexpected event (expected stream start): " &
@@ -132,8 +190,7 @@ package body Yaml.Dom.Loading is
               Data => new Document_Instance'(Refcount_Base with
                     Root_Node => null, Pool => Pool,
                 Implicit_Start => Head.Implicit_Start, Implicit_End => <>)) do
-            Ret.Data.Root_Node :=
-              Node_From (Ret.Data, Stream.Next (Input), Input, Anchors);
+            Read_Document_Content (Ret.Data, Input);
             Head := Stream.Next (Input);
             if Head.Kind /= Document_End then
                raise Stream_Error with
@@ -157,7 +214,6 @@ package body Yaml.Dom.Loading is
                            Text.Pool.With_Capacity (Text.Pool.Default_Size))
                          return Vectors.Vector is
          Head : Event := Stream.Next (Input);
-         Anchors : Anchor_Maps.Map;
       begin
          if Head.Kind /= Stream_Start then
             raise Stream_Error with "Unexpected event (expected stream start): " &
@@ -178,8 +234,7 @@ package body Yaml.Dom.Loading is
                        Implicit_Start => Head.Implicit_Start,
                        Implicit_End => <>)));
                begin
-                  Doc.Data.Root_Node :=
-                    Node_From (Doc.Data, Stream.Next (Input), Input, Anchors);
+                  Read_Document_Content (Doc.Data, Input);
                   Head := Stream.Next (Input);
                   if Head.Kind /= Document_End then
                      raise Stream_Error with
@@ -190,7 +245,6 @@ package body Yaml.Dom.Loading is
                end;
                Head := Stream.Next (Input);
                exit when Head.Kind /= Document_Start;
-               Anchors.Clear;
             end loop;
 
             if Head.Kind /= Stream_End then
